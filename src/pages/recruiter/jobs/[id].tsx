aa673d38c8fd4662b9c7d99651c7e30a3293b245
Kamran0925 <kamranrizwan0925@gmail.com>
2025-07-23 01:16:07 +0500

    Add recruitor job listings and details page

---
+++ src/pages/recruiter/jobs/[id].tsx
@@ -0,0 +1,11 @@
+// src/pages/recruiter/jobs/[id].tsx
+import RecruiterLayout from "@/layouts/RecruiterLayout/RecruiterLayout";
+import { Typography } from "@mui/material";
+
+export default function JobDetailPage() {
+  return (
+    <RecruiterLayout>
+      <Typography>Job Details Page</Typography>
+    </RecruiterLayout>
+  );
+}
